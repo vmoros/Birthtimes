e08dbf04dd016a935e196631d3377957da06a665
Viktor Moros <viktormoros@microsoft.com>
2022-06-19 15:42:02 -0400

    Rearranged the GUI. Added the ability to select multiple CSVs.

--- birthtime.py
+++ birthtime.py
@@ -1,9 +1,8 @@
 import tkinter as tk
 import csv
-from tkinter import messagebox
+from tkinter import messagebox, filedialog
 from datetime import datetime
 from tkintertable import TableCanvas
-from pathlib import Path
 from ctypes import windll
 
 
@@ -29,48 +28,76 @@ def allTimesSinceDatetime(bday: datetime):
 
 
 class Birthtimes(tk.Tk):
-    dbName = "people.csv"
     bdayFormat = "%Y-%m-%d"
 
     def __init__(self):
         windll.shcore.SetProcessDpiAwareness(2)
-        Path("people.csv").touch()
 
         super().__init__()
         windowWidth = 2 * self.winfo_screenwidth() // 3
         self.geometry(f"{windowWidth}x{self.winfo_screenheight() // 2}")
         self.title("Birthtimes ⏲️")
 
-        nameLabel = tk.Label(self, text="Name")
+        #####################################################################
+        # Section with buttons and entries: Enter name, birthday, "add person" button, "refresh" button, select CSV
+        entriesFrame = tk.Frame(self)
+        entriesFrame.pack(side="left", anchor="nw")
+        nameLabel = tk.Label(entriesFrame, text="Name")
         nameLabel.pack()
-        self.nameEntry = tk.Entry(self)
+        self.nameEntry = tk.Entry(entriesFrame)
         self.nameEntry.pack()
 
-        bdayLabel = tk.Label(self, text="Birthday (YYYY-MM-DD)")
+        bdayLabel = tk.Label(entriesFrame, text="Birthday (YYYY-MM-DD)")
         bdayLabel.pack()
-        self.bdayEntry = tk.Entry(self)
+        self.bdayEntry = tk.Entry(entriesFrame)
         self.bdayEntry.pack()
 
         addPersonButton = tk.Button(
-            self, text="Add person", command=lambda: self.refr(True)
+            entriesFrame, text="Add person", command=lambda: self.refr(True)
         )
         addPersonButton.pack()
 
-        refrButton = tk.Button(self, text="Refresh", command=lambda: self.refr(False))
+        refrButton = tk.Button(
+            entriesFrame, text="Refresh", command=lambda: self.refr(False)
+        )
         refrButton.pack()
 
-        tframe = tk.Frame(self)
-        tframe.pack(expand=True, fill=tk.BOTH)
+        csvFileLabel = tk.Label(entriesFrame, text="Selected CSV file:")
+        csvFileLabel.pack()
+        self.selectedCsv = tk.Entry(entriesFrame)
+        self.selectedCsv.pack()
+        pickCsvButton = tk.Button(
+            entriesFrame, text="Select CSV(s)", command=lambda: self.selectCsv()
+        )
+        pickCsvButton.pack()
+        #####################################################################
 
-        data = self.dataFromFile()
+        self.tableFrame = tk.Frame(self)
+        self.tableFrame.pack(side="right", anchor="e", expand=True, fill=tk.BOTH)
+
+        # data = self.dataFromFile()
         self.table = TableCanvas(
-            tframe, data=data, cellwidth=(windowWidth // 9) - 1, read_only=True
+            self.tableFrame,
+            data=dict(),
+            read_only=True,
         )
         self.table.rowheaderwidth = 0
         self.table.show()
 
-    def refr(self, addingPerson):
+    def refr(self, addingPerson: bool):
+        csvNamesRaw = self.selectedCsv.get()
+        if len(csvNamesRaw) == 0:
+            messagebox.showerror("Error", "Select at least one CSV file")
+            return
+        csvTuple = csvNamesRaw.split(",")
+
         if addingPerson:
+            if len(csvTuple) > 1:
+                messagebox.showerror(
+                    "Error", "To add a person, select only one CSV to write to"
+                )
+                return
+
             name = self.nameEntry.get()
             bday = self.bdayEntry.get()
             if len(name) == 0 or len(bday) == 0:
@@ -86,39 +113,48 @@ class Birthtimes(tk.Tk):
                 return
             self.nameEntry.delete(0, tk.END)
             self.bdayEntry.delete(0, tk.END)
-            with open(self.dbName, "a+") as f:
+
+            with open(csvNamesRaw, "a+") as f:
                 f.write(f"{name},{bday}\n")
 
-        self.table.model.importDict(self.dataFromFile())
+        self.table.model.createEmptyModel()
+        self.table.model.importDict(self.dataFromFile(csvTuple))
 
-        newColWidth = (self.winfo_width() // 9) - 1
+        newColWidth = (self.tableFrame.winfo_width() // 9) - 1
         self.table.cellwidth = newColWidth
-        for (
-            c
-        ) in (
-            self.table.model.columnwidths
-        ):  # setting the table's cellwidth won't update manually-resized columns, but this way will
+        for c in self.table.model.columnwidths:
+            # setting the table's cellwidth won't update manually-resized columns, but this way will
             self.table.model.columnwidths[c] = newColWidth
         self.table.redraw()
 
-    def dataFromFile(self):
+    def dataFromFile(self, csvTuple):
         ans = dict()
-        with open(self.dbName, newline="", mode="r+") as csvfile:
-            peopleReader = csv.reader(csvfile, delimiter=",")
-            n = 0
-            for person in peopleReader:
-                if len(person) == 2:
-                    bdayStr = person[1].strip()
-                    try:
-                        bday = datetime.strptime(bdayStr, self.bdayFormat)
-                    except:
-                        continue
 
-                    ans[n] = {"Name": person[0], "Birthday": bdayStr}
-                    ans[n].update(allTimesSinceDatetime(bday))
-                    n += 1  # TkinterTable takes data in a weird format - a dict of dicts in which the outermost keys are meaningless
+        n = 0
+        for csvName in csvTuple:
+            with open(csvName, newline="", mode="r+") as csvfile:
+                peopleReader = csv.reader(csvfile, delimiter=",")
+                for person in peopleReader:
+                    if len(person) == 2:
+                        bdayStr = person[1].strip()
+                        try:
+                            bday = datetime.strptime(bdayStr, self.bdayFormat)
+                        except:
+                            continue
+
+                        ans[n] = {"Name": person[0], "Birthday": bdayStr}
+                        ans[n].update(allTimesSinceDatetime(bday))
+                        n += 1  # TkinterTable takes data in a weird format - a dict of dicts in which the outermost keys are meaningless
 
         return ans
+
+    def selectCsv(self):
+        selectedCsvPath = filedialog.askopenfilenames(
+            filetypes=[("CSV", "*.csv")], initialdir="."
+        )
+        self.selectedCsv.delete(0, tk.END)
+        self.selectedCsv.insert(0, ",".join(selectedCsvPath))
+        self.refr(False)
 
 
 if __name__ == "__main__":
